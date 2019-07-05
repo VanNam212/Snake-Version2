1cb80d5aac8031f286aa16af05a86836cf98ae42
VanNam212 <48545931+VanNam212@users.noreply.github.com>
2019-07-05 10:52:57 +0700

    Game-Snake

--- script.js
+++ script.js
@@ -1,136 +1,152 @@
+let canvas = document.getElementById("game");
+let context = canvas.getContext('2d');
+
+let grid = 16;
+let count = 0;
 let score = 0;
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext('2d');
-let changingSnakeDirection = false;
 
-
-creatFood();
-// main();
-
-function main(){
-    setTimeout(function onTik(){
-        if(Snake.die())
-            return;
-        changingSnakeDirection = false;
-        clearCanvas();
-        Food.drawFood();
-        Snake.eat();
-        Snake.drawSnake();
-        main();
-    }, 100);
-};
-
-document.addEventListener("keydown", changeSnakeDirection);
-function clearCanvas(){
-    ctx.fillStyle = "white";
-    ctx.strokeStyle = "brown";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+//khởi tạo snake
+let snake = {
+    x: 160,
+    y: 160,
+    dx: grid,
+    dy: 0,
+    cells: [],
+    maxCells: 4
 }
 
-function randomTen(min, max){
-    return Math.round(Math.random()* ((max-min)+min) / 10) * 10;
+//khởi tạo apple
+let apple = {
+    x: 320,
+    y: 320
 }
 
-let Food = {
-    foodX : randomTen(0, canvas.width - 10),
-    foodY : randomTen(0, canvas.height - 10),
+//sinh số random int
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-    drawFood : function () {
-        ctx.fillStyle = "red";
-        ctx.fillRect(this.foodX,this.foodY,10,10);
-        ctx.stroke();
+//loop
+function loop() {
+    requestAnimationFrame(loop);
+    if (++count < 4) {
+        return;
     }
-}
+    count = 0;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    snake.x += snake.dx;
+    snake.y += snake.dy;
 
-let Snake = {
-    array_snake : [
-        {x: 150, y:75},
-        {x: 140, y:75},
-        {x: 130, y:75},
-        {x: 120, y:75},
-        {x: 110, y:75}
-    ],
+    //khi snake đụng tường thì kết thúc game
+    if (snake.x < 0) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
+        snake.dx = grid;
+        snake.dy = 0;
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
+        score = 0;
+        document.getElementById("score").innerHTML = "Score: " + score;
+        return;
+    } else if (snake.x >= canvas.width) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
+        snake.dx = grid;
+        snake.dy = 0;
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
+        score = 0;
+        document.getElementById("score").innerHTML = "Score: " + score;
+        return;
+    } else if (snake.y < 0) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
+        snake.dx = grid;
+        snake.dy = 0;
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
+        score = 0;
+        document.getElementById("score").innerHTML = "Score: " + score;
+        return;
+    } else if (snake.y >= canvas.height) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
+        snake.dx = grid;
+        snake.dy = 0;
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
+        score = 0;
+        document.getElementById("score").innerHTML = "Score: " + score;
+        return;
+    }
 
-    drawSnakePart : function (snakePart) {
-        ctx.fillStyle = "lightgreen";
-        ctx.strokeStyle= "darkgreen";
-        ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-        ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-    },
+    //thêm 1 ô vuông vào đầu snake
+    snake.cells.unshift({x: snake.x, y: snake.y});
 
-    drawSnake : function () {
-        this.array_snake.forEach(this.drawSnakePart);
-    },
+    //thêm 1 ô vuông vào đầu thì xóa ô vuông ở cuối
+    if (snake.cells.length > snake.maxCells) {
+        snake.cells.pop();
+    }
 
-    eat : function () {
-        const head = {x: this.array_snake[0].x + dx, y: this.array_snake[0].y + dy}
-        this.array_snake.unshift(head);
-        const didEatFood = head.x == Food.foodX && head.y == Food.foodY;
-        if(didEatFood){
+    //drawApple
+    context.fillStyle = 'red';
+    context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
+
+    //drawSnake
+    context.fillStyle = 'green';
+    snake.cells.forEach(function (cell, index) {
+        context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+
+        //snake eat apple
+        if (cell.x === apple.x && cell.y === apple.y) {
+            snake.maxCells++;
+            apple.x = getRandomInt(0, 25) * grid;
+            apple.y = getRandomInt(0, 25) * grid;
             score += 10;
             document.getElementById("score").innerHTML = "Score: " + score;
-            creatFood();
-        }else{
-            this.array_snake.pop();
         }
-    },
-    
-    die : function () {
-        for(var i=4; i< this.array_snake.length; i++){
-            var didItCollie = (this.array_snake[0].x === this.array_snake[i].x &&
-                this.array_snake[0].y === this.array_snake[i].y);
-            if(didItCollie)
-                return true;
-        }
-        const hitLeftWall = this.array_snake[0].x < 0;
-        const hitRightWall = this.array_snake[0].x > canvas.width - 10;
-        const hitToptWall = this.array_snake[0].y < 0;
-        const hitBottomtWall = this.array_snake[0].y > canvas.height - 10;
 
-        return hitLeftWall || hitBottomtWall || hitRightWall || hitToptWall;
-    },
-
-    move : function (event) {
-            const LEFT_KEY = 37;
-            const RIGHT_KEY = 39;
-            const UP_KEY = 38;
-            const DOWN_KEY = 40;
-
-            const keyPressed = event.keyCode;
-            const goingUp = dy === -10;
-            const goingDown = dy === 10;
-            const goingLeft = dx === -10;
-            const goingRight = dx === 10;
-            if(changingSnakeDirection)
+        //snake cắn đuôi
+        for (let i = index + 1; i < snake.cells.length; i++) {
+            if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+                snake.x = 160;
+                snake.y = 160;
+                snake.cells = [];
+                snake.maxCells = 4;
+                snake.dx = grid;
+                snake.dy = 0;
+                apple.x = getRandomInt(0, 25) * grid;
+                apple.y = getRandomInt(0, 25) * grid;
+                score = 0;
+                document.getElementById("score").innerHTML = "Score: " + score;
                 return;
-            changingSnakeDirection = true;
-            if(keyPressed == LEFT_KEY && !goingRight){
-                dx = -10;
-                dy = 0;
-            }
-            if(keyPressed == RIGHT_KEY && !goingLeft){
-                dx= 10;
-                dy=0;
-            }
-            if(keyPressed == UP_KEY && !goingDown){
-                dx = 0;
-                dy = -10;
-            }
-            if(keyPressed == DOWN_KEY && !goingUp){
-                dx = 0;
-                dy = 10;
             }
         }
+    })
 }
 
-function creatFood(){
-    Food.foodX = randomnTen(0, canvas.width - 10);
-    Food.foodY = randomnTen(0, canvas.height - 10);
-
-    Snake.array_snake.forEach(function isFoodOnSnake(part){
-        const foodIsOnSnake = part.x == Food.foodX && part.y == Food.foodY;
-        if(foodIsOnSnake)
-            creatFood();
-    });
-}
+//sự kiện ấn phím
+document.addEventListener('keydown', function (e) {
+    if (e.which === 37 && snake.dx === 0) {
+        snake.dx = -grid;
+        snake.dy = 0;
+    } else if (e.which === 38 && snake.dy === 0) {
+        snake.dy = -grid;
+        snake.dx = 0;
+    } else if (e.which === 39 && snake.dx === 0) {
+        snake.dx = grid;
+        snake.dy = 0;
+    } else if (e.which === 40 && snake.dy === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+    }
+})
+requestAnimationFrame(loop);
